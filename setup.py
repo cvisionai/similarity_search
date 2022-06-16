526e52f4f7f3417e9a82ac49d51c217fa8e8155e
Benjamin Woodward <benjamin.woodward@cvisionconsulting.com>
2022-06-16 09:26:31 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'grafit-pytorch',
+  packages = find_packages(exclude=['examples']),
+  version = '0.6.0',
+  license='MIT',
+  description = 'Grafit Pytorch Implementation',
+  author = 'Ben Woodward',
+  author_email = 'benjamin.woodward@cvisionai.com',
+  url = 'https://github.com/cvisionai/grafit-pytorch',
+  keywords = [
+      'self-supervised learning',
+      'artificial intelligence'
+  ],
+  install_requires=[
+      'torch>=1.6',
+      'torchvision>=0.8'
+  ],
+  classifiers=[
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Developers',
+      'Topic :: Scientific/Engineering :: Artificial Intelligence',
+      'License :: OSI Approved :: MIT License',
+      'Programming Language :: Python :: 3.6',
+  ],
+)
