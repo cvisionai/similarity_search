87a05dff106458de0722ba817614cedca1a4cda6
Benjamin Woodward <benjamin.woodward@cvisionconsulting.com>
2022-06-16 09:27:17 -0400

    added transforms back

--- grafit_pytorch/examples/lightning/train.py
+++ grafit_pytorch/examples/lightning/train.py
@@ -1,35 +1,36 @@
 import os
 import argparse
 import multiprocessing
-from pathlib import Path
-from PIL import Image
-
 import torch
 from torchvision import models, transforms
+from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader, Dataset
 
 from grafit_pytorch import Grafit
 import pytorch_lightning as pl
 
+from grafit_pytorch.util import FathomnetDataset
+
 # test model, a resnet 50
 
 resnet = models.resnet50(pretrained=True)
+resnet.to("cuda")
 
 # arguments
 
 parser = argparse.ArgumentParser(description='grafit-lightning-test')
 
-parser.add_argument('--image_folder', type=str, required = True,
-                       help='path to your folder of images for self-supervised learning')
+parser.add_argument('--csv_file', type=str, required = True,
+                       help='path to your csv_file containing fathomnet image information')
 
 args = parser.parse_args()
 
 # constants
 
-BATCH_SIZE = 32
+BATCH_SIZE = 16
 EPOCHS     = 1000
 LR         = 3e-4
-NUM_GPUS   = 2
+NUM_GPUS   = 1
 IMAGE_SIZE = 256
 IMAGE_EXTS = ['.jpg', '.png', '.jpeg']
 NUM_WORKERS = multiprocessing.cpu_count()
@@ -41,11 +42,13 @@ class SelfSupervisedLearner(pl.LightningModule):
         super().__init__()
         self.learner = Grafit(net, **kwargs)
 
-    def forward(self, images):
-        return self.learner(images)
+    def forward(self, sample):
+        img = sample.get('image')
+        idx = sample.get('index')
+        return self.learner(img,idx)
 
-    def training_step(self, images, _):
-        loss = self.forward(images)
+    def training_step(self, sample, _):
+        loss = self.forward(sample)
         return {'loss': loss}
 
     def configure_optimizers(self):
@@ -60,39 +63,16 @@ class SelfSupervisedLearner(pl.LightningModule):
 def expand_greyscale(t):
     return t.expand(3, -1, -1)
 
-class ImagesDataset(Dataset):
-    def __init__(self, folder, image_size):
-        super().__init__()
-        self.folder = folder
-        self.paths = []
-
-        for path in Path(f'{folder}').glob('**/*'):
-            _, ext = os.path.splitext(path)
-            if ext.lower() in IMAGE_EXTS:
-                self.paths.append(path)
-
-        print(f'{len(self.paths)} images found')
-
-        self.transform = transforms.Compose([
-            transforms.Resize(image_size),
-            transforms.CenterCrop(image_size),
-            transforms.ToTensor(),
-            transforms.Lambda(expand_greyscale)
-        ])
-
-    def __len__(self):
-        return len(self.paths)
-
-    def __getitem__(self, index):
-        path = self.paths[index]
-        img = Image.open(path)
-        img = img.convert('RGB')
-        return self.transform(img)
-
 # main
 
 if __name__ == '__main__':
-    ds = ImagesDataset(args.image_folder, IMAGE_SIZE)
+    img_transforms = transforms.Compose([transforms.Resize((256,256)),transforms.ToTensor()])
+    ds = FathomnetDataset(args.csv_file, root_dir='/mnt/md0/Projects/Fathomnet/Data_Files/2021-06-04-Download/',transform=img_transforms)
+    labels = ds.get_labels()
+    label_set = list(set(labels))
+    label_set.sort()
+    label_dict = {label : i for i,label in enumerate(label_set)}
+    labels_numeric = [label_dict.get(label) for label in labels]
     train_loader = DataLoader(ds, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS, shuffle=True)
 
     model = SelfSupervisedLearner(
@@ -101,7 +81,9 @@ if __name__ == '__main__':
         hidden_layer = 'avgpool',
         projection_size = 256,
         projection_hidden_size = 4096,
-        moving_average_decay = 0.99
+        moving_average_decay = 0.99,
+        dataset_size = len(labels),
+        dataset_labels = labels_numeric
     )
 
     trainer = pl.Trainer(
